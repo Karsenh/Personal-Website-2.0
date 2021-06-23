a1b1e14a129bce331a5ae42ba937a36fadfcc00e
Karsenh <KarsenHansen@gmail.com>
2021-06-23 02:35:35 -0700

    created custom landing header css class

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <Landing>
         <h1>About me</h1>
+        <hr />
+        <br />
         <h1>Portfolio</h1>
         <h1>Contact</h1>
       </Landing>
